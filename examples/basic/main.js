adb4f0f269d4c1e1a0a80c03ab45c1d0b1187299
Johnny Wu <KarasAya@gmail.com>
2017-04-25 13:33:13 +0800

    add example

--- examples/basic/main.js
+++ examples/basic/main.js
@@ -3,15 +3,24 @@
 const {app, BrowserWindow} = require('electron');
 const selection = require('../../index');
 
-let win;
+let win1, win2;
 
 app.on('ready', function () {
-  win = new BrowserWindow({
-    center: true,
+  win1 = new BrowserWindow({
+    x: 100,
+    y: 100,
+    width: 600,
+    height: 600,
+  });
+  win1.loadURL('file://' + __dirname + '/win1.html');
+
+  win2 = new BrowserWindow({
+    x: 710,
+    y: 100,
     width: 400,
     height: 300,
   });
-  win.loadURL('file://' + __dirname + '/index.html');
+  win2.loadURL('file://' + __dirname + '/win2.html');
 
   selection.register('normal');
 });
